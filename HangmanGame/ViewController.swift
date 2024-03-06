05741ff96837de1aa25b61fe9811cc791b289443
o-pylypiv <bassenia@gmail.com>
2024-03-06 14:04:25 +0200

    Initial Commit

---
+++ HangmanGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HangmanGame
+//
+//  Created by Olha Pylypiv on 06.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
